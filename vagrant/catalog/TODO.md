f05034b05b166b2da1de0bb5fd8690618cb5271b
Dave Voutila <voutilad@gmail.com>
2015-06-12 17:41:33 -0700

    bower, bunch of fixes, added item page

---
+++ vagrant/catalog/TODO.md
@@ -0,0 +1,16 @@
+# Primary Requirements
+
+* Latest items displayed on landing page - name/category
+  * Add item count to each category
+  * Add category membership to item listing
+* Display selected categories items alongside categories
+* Detail page on an Item
+* Delete confirmation page
+* Edit item page
+* OAuth login
+* JSON REST API
+
+Bonus:
+* upload/add image to item or category
+* extra comments on items/categories
+* add nonce's to delete POST - https://clipperz.is/security_privacy/crypto_algorithms/
--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -4,14 +4,15 @@ from database_setup import Category, Item, session
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/catalog')
 def index():
     s = session()
     categories = s.query(Category).all()
+    items = s.query(Item).limit(20).all()
 
-    return render_template('main.j2', categories=categories)
+    return render_template('catalog.j2', categories=categories, items=items)
 
-@app.route('/category/<int:category_id>')
+@app.route('/catalog/<int:category_id>')
 def showCategory(category_id):
     s = session()
     category = s.query(Category).filter(Category.id == category_id).one()
@@ -19,19 +20,27 @@ def showCategory(category_id):
 
     return render_template('category.j2', category=category, items=items)
 
-@app.route('/category/<int:category_id>/create')
+@app.route('/catalog/category/<int:category_id>/create')
 def newCategory(category_id):
     return 'New category page with id: ' + str(category_id)
 
-@app.route('/category/<int:category_id>/delete')
+@app.route('/catalog/category/<int:category_id>/delete')
 def deleteCategory(category_id):
     return 'Delete page for category id: ' + str(category_id)
 
-@app.route('/category/<int:category_id>/edit')
+@app.route('/catalog/category/<int:category_id>/edit')
 def editCategory(category_id):
     return 'Edit page for category_id: ' + str(category_id)
 
+@app.route('/catalog/item/<int:item_id>')
+def viewItem(item_id):
+    s = session()
+    item = s.query(Item).filter(Item.id == item_id).one()
+    return render_template('item.j2', item=item)
 
+@app.route('/catalog/item/<int:item_id>/edit')
+def editItem(item_id):
+    return 'Edit page for item with id: ' + str(item_id)
 
 if __name__ == '__main__':
     config = ConfigParser.SafeConfigParser(
