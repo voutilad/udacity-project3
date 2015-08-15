2ecbe7796b6fe97d6516c922e19320f87cfdccd1
voutilad <voutilad@gmail.com>
2015-08-15 13:03:12 -0400

    wired in login link and display on navbar

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -22,7 +22,9 @@ def home():
         counts.update({category.id : db.getItemCount(category.id)})
     items = db_session.query(Item).order_by(Item.modified_date.desc()).limit(20).all()
 
-    return render_template('catalog.j2', categories=categories, items=items, counts=counts)
+    return render_template('catalog.j2', categories=categories,
+                           items=items, counts=counts,
+                           login_session=login_session)
 
 ### session handling & login
 
@@ -31,7 +33,9 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.j2', client_id=app.client_id, state=state)
+    return render_template('login.j2',
+                           client_id=app.client_id, state=state,
+                           login_session=login_session)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -116,7 +120,10 @@ def showCategory(category_id):
         return jsonify(category=category.to_json(),
                        items=[i.to_json() for i in items])
     else:
-        return render_template('category.j2', category=category,items=items)
+        return render_template('category.j2',
+                               category=category,
+                               items=items,
+                               login_session=login_session)
 
 @app.route('/catalog/category/new', methods=['GET', 'POST'])
 def newCategory():
@@ -125,9 +132,11 @@ def newCategory():
         description = request.form['description']
         category_id = utils.slugify(name)
         db.putCategory(Category(name=name, id=category_id, description=description))
-        return redirect(url_for('showCategory', category_id=category_id))
+        return redirect(url_for('showCategory',
+                                category_id=category_id,
+                                login_session=login_session))
     else:
-        return render_template('category-new.j2')
+        return render_template('category-new.j2', login_session=login_session)
 
 @app.route('/catalog/category/<string:category_id>/delete', methods=['GET', 'POST'])
 def deleteCategory(category_id):
@@ -137,11 +146,14 @@ def deleteCategory(category_id):
     if request.method == 'GET':
         if items:
             print '[DEBUG]: category ' + category.id + ' has items. Need confirmation.'
-            return render_template('category-delete.j2', category=category, items=items)
+            return render_template('category-delete.j2',
+                                   category=category,
+                                   items=items,
+                                   login_session=login_session)
         else:
             print '[DEBUG]: category ' + category.id + ' empty. Deleting.'
             db.deleteCategory(category)
-            return redirect(url_for('home'))
+            return redirect(url_for('home', login_session=login_session))
     else: #POST
         if items:
             db.deleteItems(items)
@@ -149,7 +161,7 @@ def deleteCategory(category_id):
         if utils.request_wants_json(request):
             return 'OK' #TODO
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('home', login_session=login_session))
 
 @app.route('/catalog/category/<string:category_id>/edit')
 def editCategory(category_id):
@@ -165,15 +177,20 @@ def newItem(category_id):
         item_id = utils.slugify(item_name)
         db.putItem(Item(name=item_name, description=item_description,
                    id=item_id, category_id=category_id))
-        return redirect(url_for('showCategory', category_id=category_id))
+        return redirect(url_for('showCategory',
+                                category_id=category_id,
+                                login_session=login_session))
     else:
         category = db.getCategory(category_id)
-        return render_template('item-new.j2', category_name=category.name,
-                               category_id=category_id)
+        return render_template('item-new.j2',
+                               category_name=category.name,
+                               category_id=category_id,
+                               login_session=login_session)
 
 @app.route('/catalog/<string:category_id>/<string:item_id>')
 def viewItem(item_id, category_id):
-    return render_template('item.j2', item=db.getItem(item_id, category_id))
+    return render_template('item.j2', item=db.getItem(item_id, category_id),
+                           login_session=login_session)
 
 @app.route('/catalog/<string:category_id>/<string:item_id>/update', methods=['GET', 'POST'])
 def updateItem(item_id, category_id):
@@ -187,17 +204,21 @@ def updateItem(item_id, category_id):
             changes.update({'description':description})
 
         db.updateItem(Item(id=item_id, category_id=category_id), changes)
-        return redirect(url_for('viewItem', item_id=item_id, category_id=category_id))
+        return redirect(url_for('viewItem', item_id=item_id,
+                                category_id=category_id,
+                                login_session=login_session))
     else:
         return render_template('item-editor.j2',
                                item=db.getItem(item_id, category_id),
-                               categories=db.getCategories())
+                               categories=db.getCategories(),
+                               login_session=login_session)
 
 @app.route('/catalog/<string:category_id>/<string:item_id>/delete', methods=['GET', 'POST'])
 def deleteItem(item_id, category_id):
     if request.method == 'POST':
         db.deleteItem(item_id, category_id)
-        return redirect(url_for('showCategory', category_id=category_id))
+        return redirect(url_for('showCategory', category_id=category_id,
+                                login_session=login_session))
     else:
         return 'TODO! Sup girl. You want a delete confirmation or something?'
 
