c268a2b4faa926c37646829c51b220be2acc5156
voutilad <voutilad@gmail.com>
2015-08-15 12:46:16 -0400

    got oauth2 working with google

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -1,10 +1,15 @@
 import ConfigParser
 from flask import Flask, render_template, request, url_for, redirect, jsonify
-from flask import session as login_session
 import random, string
 from models import Category, Item
 from database import db_session
 import db, utils
+
+### security stuff
+from flask import session as login_session, make_response
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import httplib2, json, requests
+
 app = Flask(__name__)
 
 
@@ -26,8 +31,80 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.j2', client_id=app.client_id)
+    return render_template('login.j2', client_id=app.client_id, state=state)
 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state!'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        msg = 'Failed to upgrade the authorization code.'
+        response = make_response(json.dumps(msg), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error'), 501))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    gplus_id = credentials.id_token['sub']
+
+    if result['user_id'] != gplus_id:
+        msg = "Token's user ID doesn't match."
+        response = make_response(json.dumps(msg), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    if result['issued_to'] != app.client_id:
+        msg = "Token's client ID doesn't match applications"
+        response = make_response(json.dumps(msg), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        msg = 'Current user is already connected'
+        response = make_response(json.dumps(msg), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    login_session['credentials'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    #Creep on the user
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    print("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 ### categories
 
 @app.route('/catalog/<string:category_id>')
