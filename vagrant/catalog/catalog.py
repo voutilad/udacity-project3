cf46466d21e133d1882fe27927d51f933606541b
Dave Voutila <voutilad@gmail.com>
2015-10-16 12:55:33 -0400

    added basic disconnect capability with /gdisconnect

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -109,6 +109,37 @@ def gconnect():
     print("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    if login_session is None or not login_session.has_key('credentials'):
+        response = make_response(json.dumps('User not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    credentials = login_session['credentials']
+
+    #access_token = credentials.access_token
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 ### categories
 
 @app.route('/catalog/<string:category_id>')
